6f07c53dddb7226b23061c2b079a523e5cdd2453
Maryna <veretiuk@gmail.com>
2026-05-24 20:06:16 -0400

    fix feedback issues

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -75,6 +75,12 @@ export const refreshUserSession = async (req, res, next) => {
     }
 
     if (new Date() > session.refreshTokenValidUntil) {
+      await Session.deleteOne({ _id: session._id });
+
+      res.clearCookie('sessionId');
+      res.clearCookie('accessToken');
+      res.clearCookie('refreshToken');
+
       throw createHttpError(401, 'Session token expired');
     }
 
